7fa3a74ab3bbdec05b2e704e1e3e869746c65a55
Aswin Krishna <116379127+TheProv1@users.noreply.github.com>
2022-10-23 00:53:29 +0400

    Update db_and_table_checker.py

--- db_and_table_checker.py
+++ db_and_table_checker.py
@@ -1,3 +1,11 @@
 import package_checker
 
-conobj = mysql.connector.connect(user = usr, )
+passwd = input("Enter the MySQL Command Line Client Password: ")
+
+conobj = mysql.connector.connect(host = 'localhost', user = 'root', password = passwd)
+
+if conobj.is_connected:
+	print("Connected Successfully")
+else:
+	print("Password Entered is Incorrect")
+	exit()
